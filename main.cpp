eeada7c1161ab25e51cf4b69ba6ea27d0365fcb9
Feng Chen <feng@Fengs-MacBook-Air.local>
2023-08-25 14:29:59 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+
+void fillCommands(unordered_set<string>& commands){
+    string commandsFile = "commands.txt";
+    ifstream fin(commandsFile);
+    while (true){
+        string s; fin >> s;
+        if (fin.eof()){
+            break;
+        }
+        commands.insert(s);
+    }
+}
+
+
+
+int main(){
+    unordered_set<string> commands;
+    fillCommands(commands);
+    while (true){
+        cout << "Enter command:\n";
+        string command; cin >> command;
+        if (commands.find(command) == commands.end()){
+            cout << "Please enter in a valid command. Type 'help' to view commands\n";
+        }
+    }
+    return 0;
+}
